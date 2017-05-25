d1f4ee7277aef21c4dd4d590d8c09d2ab7a0d276
Nathan New <nathanhnew@gmail.com>
2017-05-25 17:02:31 -0500

    css edits header banner

---
+++ blog/templatetags/base_extras.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def active_page(request, view_name):
+
+	from django.core.urlresolvers import resolve, Resolver404
+	path = resolve(request.path_info)
+	if not request:
+		return "titties"
+	try:
+		return "active" if path.url_name == view_name else ""
+	except Resolver404:
+		return "welp"
